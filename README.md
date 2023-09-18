a04196e9b5783069b4e0f2d1085b33ab7a9984d1
pirately <57277605+pirately@users.noreply.github.com>
2023-09-19 07:01:09 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# Build-OpenWrt
+
+## 云编译OpenWRT固件
+
+[![Lean](https://img.shields.io/badge/OpenWrt-Lede-blueviolet.svg?style=flat&logo=appveyor)](https://github.com/coolsnowwolf/lede)  [![immortalwrt](https://img.shields.io/badge/OpenWrt-ImmortalWrt-orange.svg?style=flat&logo=appveyor)](https://github.com/immortalwrt/immortalwrt)  [![OpenWrt](https://img.shields.io/badge/OpenWrt-Official-red.svg?style=flat&logo=appveyor)](https://github.com/openwrt/openwrt)
+
+## 固件简要说明：
+
+固件每天早上2点，检查上游源代码，自动编译。
+
+固件信息里的时间为编译开始的时间，方便核对上游源码提交时间。
+
+## 目录简要说明：
+
+workflows——actions脚本
+
+config——自定义配置
+
+- general.config 为通用配置文件，用于设定各平台都用得到的插件。
+
+- x86_64.config为amd64设备主要配置文件，其他机型需要添加。
+
+scripts——自定义脚本
+
+settings——设置文件
+
+- depends-ubuntu——环境依赖列表
+- settings.ini——设置源码以及分支等
+
+### 使用方法：
+
+需要在`Token`里面添加如下信息：
+
+- REPO_TOKEN
+
+  > REPO_TOKEN密匙制作教程：https://github.com/danshui-git/shuoming/blob/master/jm.md
+
+
+- SCKEY
+
+  > ServerChan工具的token教程：
+  >
+  > 1、获取SCKEY：[点击这里](https://sct.ftqq.com/login)，微信扫码，扫码后再点登录（需要两步），就可以看到你的token了
+  >
+  > 2、绑定微信：点击导航栏的 [微信推送]，用微信扫描二维码关注公众号并在页面中 [检查结果并确认绑定] 即可。
+  >
+  > 3、测试一下：直接在 [发送消息] 页面的在线发送消息工具中填写标题和消息内容，点击发送消息就可以测试通知效果了。
+  > 
+  > 4、添加token：复制好你的`SCKEY`后，接下来到你自己的仓库，点Settings，再点左边的Secrets，然后点右上角的New repositonry secret，然后在Name下面的方框写上名字，名字为（SCKEY）不包括括号，Value下面大方框放进密匙，点下面的绿色按钮Add secret保存即完成
---
+++ scripts/diy_op.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+#加入作者信息, %Y表示4位数年份如2023, %y表示2位数年份如23
+if [[ $OWRT_URL == *"lede"* ]] ; then
+  sed -i "s/DISTRIB_DESCRIPTION='*.*'/DISTRIB_DESCRIPTION='OpenWrt by Jeffen'/g" package/lean/default-settings/files/zzz-default-settings   
+  sed -i "s/DISTRIB_REVISION='*.*'/DISTRIB_REVISION=' $(date +%Y.%m.%d)'/g" package/lean/default-settings/files/zzz-default-settings
+else
+  sed -i "s/DISTRIB_DESCRIPTION='*.*'/DISTRIB_DESCRIPTION='OpenWrt by Jeffen'/g" package/base-files/files/etc/openwrt_release
+  sed -i "s/DISTRIB_REVISION='*.*'/DISTRIB_REVISION=' $(date +%Y.%m.%d)'/g" package/base-files/files/etc/openwrt_release
+fi
+
+# 安装bash
+echo "CONFIG_PACKAGE_bash=y" >> .config
+
+# OpenWrt官方HaProxy
+svn co https://github.com/openwrt/packages/branches/openwrt-23.05/net/haproxy
+rm -rf feeds/packages/net/haproxy
+mv haproxy feeds/packages/net
+
+#删除自带的passwall
+rm -rf feeds/luci/applications/luci-app-passwall
+#删除自带的packages
+rm -rf feeds/packages/net/hysteria
+rm -rf feeds/packages/net/sing-box
+#用lede的ssrplus插件还是passwall插件，用ssrplus插件并且固件是lede的话，更新haproxy
+if [[ $OPENWRT_APPLICATIONS == "passwall" ]] ; then
+  #增加luci界面
+  echo "CONFIG_PACKAGE_luci-app-passwall=y" >> .config
+  echo "CONFIG_PACKAGE_luci-app-passwall_INCLUDE_V2ray_Geodata=y" >> .config
+fi
+if [[ $OPENWRT_APPLICATIONS == "ssrplus" ]] ; then
+  if [[ $OWRT_URL == *"lede"* ]] ; then
+    git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
+  fi
+  if [[ $OWRT_SOURCE == "immortalwrt" ]]; then
+    svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus
+    rm -rf feeds/luci/applications/luci-app-ssr-plus
+    mv luci-app-ssr-plus feeds/luci/applications
+  fi
+  #增加luci界面
+  echo "CONFIG_PACKAGE_luci-app-ssr-plus=y" >> .config
+  echo "CONFIG_PACKAGE_haproxy=y" >> .config
+fi
+#openclash插件
+if [[ $OPENWRT_APPLICATIONS == "openclash" ]] ; then
+  rm -rf feeds/luci/applications/luci-app-openclash
+  #增加luci界面
+  echo "CONFIG_PACKAGE_luci-app-openclash=y" >> .config
+fi
+
+# BBR
+if [[ $OWRT_URL == *"lede"* ]] ; then
+  sed -i "s/option bbr_cca '0'/option bbr_cca '1'/g" feeds/luci/applications/luci-app-turboacc/root/etc/config/turboacc
+fi
+
+# 配置网络环境
+# 旁路由
+if [[ $OWRT_URL == *"lede"* ]] ; then
+  sed -i '$i uci set network.lan.ifname="eth0"' package/lean/default-settings/files/zzz-default-settings
+  sed -i '$i uci set network.lan.gateway="10.0.1.2"' package/lean/default-settings/files/zzz-default-settings
+  sed -i '$i uci set network.lan.dns="223.5.5.5"' package/lean/default-settings/files/zzz-default-settings
+  sed -i '$i uci commit network' package/lean/default-settings/files/zzz-default-settings
+fi
+if [[ $OWRT_SOURCE == "immortalwrt" ]]; then
+  sed -i '$i uci set network.lan.ifname="eth0"' package/emortal/default-settings/files/99-default-settings
+  sed -i '$i uci set network.lan.gateway="10.0.1.2"' package/emortal/default-settings/files/99-default-settings
+  sed -i '$i uci set network.lan.dns="223.5.5.5"' package/emortal/default-settings/files/99-default-settings
+  sed -i '$i uci commit network' package/emortal/default-settings/files/99-default-settings
+fi
