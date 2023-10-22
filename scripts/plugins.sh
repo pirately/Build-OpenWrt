09a84756358703837eba2b65ddc641461eecbbfb
pirately <57277605+pirately@users.noreply.github.com>
2023-10-22 22:20:16 +0800

    update

--- scripts/plugins.sh
+++ scripts/plugins.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 #Design Theme
-git clone --depth=1 --single-branch --branch $(echo $OWRT_URL | grep -iq "lede" && echo "main" || echo "js") https://github.com/gngpp/luci-theme-design.git
+if [[ $OWRT_URL == *"lede"* ]] ; then
+  git clone --depth=1 --single-branch --branch "main" https://github.com/gngpp/luci-theme-design.git
+else
+  git clone --depth=1 --single-branch --branch "js" https://github.com/papagaye744/luci-theme-design.git
+fi
 git clone --depth=1 --single-branch https://github.com/gngpp/luci-app-design-config.git
 #Argon Theme
 git clone --depth=1 --single-branch --branch $(echo $OWRT_URL | grep -iq "lede" && echo "18.06" || echo "master") https://github.com/jerrykuku/luci-theme-argon.git
@@ -14,7 +18,10 @@ git clone --depth=1 --single-branch https://github.com/linkease/nas-packages-luc
 git clone --depth=1 --single-branch --branch "dev" https://github.com/vernesong/OpenClash.git
 #Pass Wall
 git clone --depth=1 --single-branch https://github.com/xiaorouji/openwrt-passwall.git
+git clone --depth=1 --single-branch https://github.com/xiaorouji/openwrt-passwall2.git
 git clone --depth=1 --single-branch https://github.com/xiaorouji/openwrt-passwall-packages.git
+#Hello World
+git clone --depth=1 --single-branch https://github.com/fw876/helloworld.git
 
 #预置OpenClash内核和GEO数据
 export CORE_VER=https://raw.githubusercontent.com/vernesong/OpenClash/core/dev/core_version
@@ -28,12 +35,14 @@ export TUN_VER=$(curl -sfL $CORE_VER | sed -n "2{s/\r$//;p;q}")
 export GEO_MMDB=https://github.com/alecthw/mmdb_china_ip_list/raw/release/lite/Country.mmdb
 export GEO_SITE=https://github.com/Loyalsoldier/v2ray-rules-dat/raw/release/geosite.dat
 export GEO_IP=https://github.com/Loyalsoldier/v2ray-rules-dat/raw/release/geoip.dat
+export META_DB=https://github.com/MetaCubeX/meta-rules-dat/raw/release/geoip.metadb
 
 cd ./OpenClash/luci-app-openclash/root/etc/openclash
 
 curl -sfL -o ./Country.mmdb $GEO_MMDB
 curl -sfL -o ./GeoSite.dat $GEO_SITE
 curl -sfL -o ./GeoIP.dat $GEO_IP
+curl -sfL -o ./GeoIP.metadb $META_DB
 
 mkdir ./core && cd ./core
 
