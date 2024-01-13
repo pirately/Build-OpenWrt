a86ef856b03b493620a48a8633ab2020aa6f8709
pirately <57277605+pirately@users.noreply.github.com>
2024-01-13 15:40:10 +0800

    更新 diy_op.sh

--- scripts/diy_op.sh
+++ scripts/diy_op.sh
@@ -22,6 +22,8 @@ fi
 # 删除自带的passwall
 rm -rf feeds/luci/applications/luci-app-passwall
 # 删除自带的packages
+rm -rf feeds/packages/net/xray-core
+rm -rf feeds/packages/net/xray-plugin
 rm -rf feeds/packages/net/hysteria
 rm -rf feeds/packages/net/sing-box
 # 相关插件
