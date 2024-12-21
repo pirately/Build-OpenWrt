24e36b1761a52a438b6815f27249d0922458e8d1
pirately <57277605+pirately@users.noreply.github.com>
2024-12-21 23:28:09 +0800

    update

--- scripts/diy_op.sh
+++ scripts/diy_op.sh
@@ -27,7 +27,7 @@ fi
 # 删除自带的packages
 # rm -rf feeds/luci/applications/luci-app-passwall
 rm -rf feeds/packages/net/chinadns-ng
-rm -rf feeds/packages/net/xray-core
+# rm -rf feeds/packages/net/xray-core
 # rm -rf feeds/packages/net/xray-plugin
 # rm -rf feeds/packages/net/hysteria
 # rm -rf feeds/packages/net/sing-box
