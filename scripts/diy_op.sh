ba888a3994baa6660913be62aa789e25faa895e4
pirately <57277605+pirately@users.noreply.github.com>
2024-12-21 19:25:22 +0800

    update

--- scripts/diy_op.sh
+++ scripts/diy_op.sh
@@ -25,12 +25,12 @@ if [[ $WRT_URL == *"lede"* ]] ; then
 fi
 
 # 删除自带的packages
-rm -rf feeds/luci/applications/luci-app-passwall
-rm -rf feeds/packages/net/chinadns-ng
-rm -rf feeds/packages/net/xray-core
-rm -rf feeds/packages/net/xray-plugin
-rm -rf feeds/packages/net/hysteria
-rm -rf feeds/packages/net/sing-box
+# rm -rf feeds/luci/applications/luci-app-passwall
+# rm -rf feeds/packages/net/chinadns-ng
+# rm -rf feeds/packages/net/xray-core
+# rm -rf feeds/packages/net/xray-plugin
+# rm -rf feeds/packages/net/hysteria
+# rm -rf feeds/packages/net/sing-box
 
 # 相关插件
 if [[ $OPENWRT_APPLICATIONS == "passwall" ]] ; then
