105d18994162b7bb843648064157825e4aecb526
pirately <57277605+pirately@users.noreply.github.com>
2024-12-23 15:09:19 +0800

    update

--- scripts/diy_op.sh
+++ scripts/diy_op.sh
@@ -61,7 +61,6 @@ if [[ $OPENWRT_APPLICATIONS == "passwall" ]] ; then
 fi
 if [[ $OPENWRT_APPLICATIONS == "passwall2" ]] ; then
   # 增加luci界面
-  # echo "CONFIG_PACKAGE_geoview=y" >> .config
   echo "CONFIG_PACKAGE_luci-app-passwall2=y" >> .config
 fi
 if [[ $OPENWRT_APPLICATIONS == "ssrplus" ]] ; then
